f17dc3a5b3c6b4b3368f303c2aa760d64fd9be58
AquilesMendozaTasayco <aquilesfabianmendozatasayco@gmail.com>
2026-04-23 14:47:26 -0500

    update

--- components/BotonWhatsApp.jsx
+++ components/BotonWhatsApp.jsx
@@ -6,7 +6,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function BotonWhatsApp() {
   return (
     <a
-      href="https://wa.me/51979650999"
+      href="https://wa.me/51900241682"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-5 right-5 z-50 bg-[#25D366] hover:bg-[#1ebe5d] text-white rounded-full w-14 h-14 flex items-center justify-center shadow-lg transition-all duration-300 hover:scale-110"
